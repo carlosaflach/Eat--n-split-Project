1b6f37e315de0a63a30321787f0fd1727e820481
Carlos Afonso <carlos.flach15@gmail.com>
2023-07-11 22:55:14 -0300

    feat: add emojis

--- src/Components/FormAddFriend.tsx
+++ src/Components/FormAddFriend.tsx
@@ -4,10 +4,10 @@ import Button from './Button';
 const FormAddFriend = () => {
   return (
     <form className='form-add-friend'>
-      <label>Friend name</label>
+      <label>👫 Friend name</label>
       <input type='text' />
 
-      <label>Image URL</label>
+      <label>🌄 Image URL</label>
       <input type='text' />
       <Button>Add</Button>
     </form>
--- src/Components/FormSplitBill.tsx
+++ src/Components/FormSplitBill.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Button from './Button';
 
 const FormSplitBill = () => {
   return (
     <form className='form-split-bill'>
       <h2>Split a bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type='text' />
+
+      <label>🧍‍♀️ Your expense</label>
+      <input type='text' />
+
+      <label>👫 X's expense</label>
+      <input type='text' disabled />
+
+      <label>🤑 Who is paying the bill</label>
+
+      <Button>Split Bill</Button>
     </form>
   );
 };
