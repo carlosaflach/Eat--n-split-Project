5ec473d3c4e2cdefd960f1f5d6517f1dbf92b61f
Carlos Afonso <carlos.flach15@gmail.com>
2023-07-11 22:35:03 -0300

    feat: create a form add friend component

---
+++ src/Components/FormAddFriend.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from './Button';
+
+const FormAddFriend = () => {
+  return (
+    <form className='form-add-friend'>
+      <label htmlFor=''>Friend name</label>
+      <input type='text' />
+
+      <label>Image URL</label>
+      <input type='text' />
+      <Button>Add</Button>
+    </form>
+  );
+};
+
+export default FormAddFriend;
