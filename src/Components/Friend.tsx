a0abae50e1ec41bc4351f3e17884d41eb2aabb6a
Carlos Afonso <carlos.flach15@gmail.com>
2023-07-11 22:30:23 -0300

    feat: creates a friend component

--- src/Components/Friend.tsx
+++ src/Components/Friend.tsx
@@ -1,10 +1,30 @@
 import { FC } from 'react';
+import { IFriends } from '../App';
 
 interface IFriendProps {
-  friend: any;
+  friend: IFriends;
 }
 
 const Friend: FC<IFriendProps> = ({ friend }) => {
-  return <li>{friend.name}</li>;
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className='red'>
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className='green'>
+          {friend.name} owes you {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <button className='button'>Select</button>
+    </li>
+  );
 };
 export default Friend;
